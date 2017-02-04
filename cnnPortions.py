1cba76739a77c929622583c6fa652ad7c02b9e2f
Dane Bell <dane@email.arizona.edu>
2017-02-04 14:26:35 -0700

    Bug fix

--- cnnPortions.py
+++ cnnPortions.py
@@ -401,7 +401,7 @@ for start in range(0, 95, 5):
     predmn = (predmn >= 0.5).astype(int)
     
     # weight by recency (most recent tweets first)
-    wts = np.linspace(1., 0.01, maxtweets)
+    wts = np.linspace(1., 0.01, train_shp[1])
     predwm = np.average(pred, axis=1, weights=wts)
     predwm = (predwm >= 0.5).astype(int)
 
