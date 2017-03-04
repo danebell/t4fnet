c855835cf55b31e3cf238ba9352e1515c505bcac
Laparra <laparra@SBS-SBS5236.local>
2017-03-03 17:47:28 -0700

    New changes from dane

--- owTextClassifier3D.py
+++ owTextClassifier3D.py
@@ -23,7 +23,7 @@ from keras.layers import Convolution1D, MaxPooling1D, Flatten
 from keras.layers.core import K
 
 
-# In[21]:
+# In[2]:
 
 def pad3d(sequences, maxtweets=None, maxlen=None, dtype='int32',
           padding='pre', truncating='pre', value=0.):
@@ -87,8 +87,7 @@ def push_indices(x, start, index_from):
         return x
 
 def load_data(path='ow3d.pkl', nb_words=None, skip_top=0,
-              maxlen=None, seed=113,
-              start=1, oov=2, index_from=3):
+              maxlen=None, seed=113, start=1, oov=2, index_from=3):
     '''
     # Arguments
         path: where the data is stored (in '.')
@@ -109,7 +108,9 @@ def load_data(path='ow3d.pkl', nb_words=None, skip_top=0,
     words that were present in the training set but are not included
     because they're not making the `nb_words` cut here.
     Words that were not seen in the training set but are in the test set
-    have simply been skipped.
+    have simply been skipped. See preprocessText3D
+    
+    Adapted from keras.datasets.imdb.py by François Chollet
     '''
     
     if path.endswith(".gz"):
@@ -117,53 +118,53 @@ def load_data(path='ow3d.pkl', nb_words=None, skip_top=0,
     else:
         f = open(path, 'rb')
 
-    (train_X, train_y) = pkl.load(f)
-    (test_X, test_y) = pkl.load(f)
+    (x_pos, y_pos) = pkl.load(f)
+    (x_neg, y_neg) = pkl.load(f)
 
     f.close()
 
     # randomize datum order
     np.random.seed(seed)
-    np.random.shuffle(train_X)
+    np.random.shuffle(x_pos)
     np.random.seed(seed)
-    np.random.shuffle(train_y)
+    np.random.shuffle(y_pos)
 
     np.random.seed(seed * 2)
-    np.random.shuffle(test_X)
+    np.random.shuffle(x_neg)
     np.random.seed(seed * 2)
-    np.random.shuffle(test_y)
+    np.random.shuffle(y_neg)
     
     # keep maxlen words of each tweet
     if maxlen is not None:
-        train_X = cap_length(train_X, maxlen)
-        test_X = cap_length(test_X, maxlen)
+        x_pos = cap_length(x_pos, maxlen)
+        x_neg = cap_length(x_neg, maxlen)
 
     # cut off infrequent words to vocab of size nb_words
     if nb_words is not None:
-        train_X = cap_words(train_X, nb_words, oov)
-        test_X = cap_words(test_X, nb_words, oov)
+        x_pos = cap_words(x_pos, nb_words, oov)
+        x_neg = cap_words(x_neg, nb_words, oov)
 
     # cut off most frequent skip_top words
     if skip_top > 0:
-        train_X = skip_n(train_X, skip_top, oov)
-        test_X = skip_n(test_X, skip_top, oov)
+        x_pos = skip_n(x_pos, skip_top, oov)
+        x_neg = skip_n(x_neg, skip_top, oov)
 
     # prepend each sequence with start and raise indices by index_from
-    train_X = push_indices(train_X, start, index_from)
-    test_X = push_indices(test_X, start, index_from)
+    x_pos = push_indices(x_pos, start, index_from)
+    x_neg = push_indices(x_neg, start, index_from)
     
-    train_X = np.array(train_X)
-    train_y = np.array(train_y)
+    x_pos = np.array(x_pos)
+    y_pos = np.array(y_pos)
 
-    test_X = np.array(test_X)
-    test_y = np.array(test_y)
+    x_neg = np.array(x_neg)
+    y_neg = np.array(y_neg)
     
-    return (train_X, train_y), (test_X, test_y)
+    return (x_pos, y_pos), (x_neg, y_neg)
 
 
 def load_embeddings(nb_words=None, emb_dim=200, index_from=3,
                     vocab='ow3d.dict.pkl', 
-                    w2v='food_vectors_clean.txt'):
+                    w2v='/data/nlp/corpora/twitter4food/food_vectors_clean.txt'):
 
     f = open(vocab, 'rb')
     word_index = pkl.load(f)
@@ -253,8 +254,78 @@ def bootstrap(gold, pred, reps=100000):
         stars = '***'
     else:
         stars = ''
+        
     acc = np.mean(agr)
+    tp = 0
+    tn = 0
+    fn = 0
+    fp = 0
+    for i in range(len(gold)):
+        if gold[i] == 1 and pred[i] == 1:
+            tp = tp + 1
+        elif gold[i] == 1 and pred[i] != 1:
+            fn = fn + 1
+        elif gold[i] != 1 and pred[i] == 1:
+            fp = fp +1
+        else:
+            tn = tn +1
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+
+    f1s = []
+    tps = []
+    tns = []
+    fns = []
+    fps = []
+    for lbl in (hist.keys()):
+        if lbl == -1:
+            continue
+        tp = 0
+        tn = 0
+        fn = 0
+        fp = 0
+        for i in range(len(gold)):
+            if gold[i] == lbl and pred[i] == lbl:
+                tp = tp + 1
+            elif gold[i] == lbl and pred[i] != lbl:
+                fn = fn + 1
+            elif gold[i] != lbl and pred[i] == lbl:
+                fp = fp +1
+            else:
+                tn = tn +1
+
+        tps.append(tp)
+        tns.append(tn)
+        fns.append(fn)
+        fps.append(fp)
+        if (tp + fp == 0):
+            prec = 0
+        else:
+            prec = tp / (tp + fp)
+        if (tp + fn == 0):
+            rec = 0
+        else:
+            rec = tp / (tp + fn)
+        if (prec + rec == 0):
+            f1s.append(0)
+        else:
+            f1s.append(2 * prec * rec / (prec + rec))
+
+    f1s = np.array(f1s)
+    tps = np.array(tps)
+    tns = np.array(tns)
+    fns = np.array(fns)
+    fps = np.array(fps)
+    prec = tps.sum() / (tps.sum() + fps.sum())
+    rec = tps.sum() / (tps.sum() + fns.sum())
+    microf1 = 2 * prec * rec / (prec + rec)
+    macrof1 = f1s.sum() / len(f1s)
+    
     print('accuracy = %.4f' % acc)
+    print('precision = %.4f' % precision)
+    print('recall = %.4f' % recall)
+    print('microF1 = %.4f' % microf1)
+    print('macroF1 = %.4f' % macrof1)
     print('baseline = %.4f' % baseline)
     print('p = %.6f%s' % (p, stars))
     return (acc, baseline, p)
@@ -265,18 +336,40 @@ def bootstrap(gold, pred, reps=100000):
 # The data must be loaded, padded, and reshaped to train a tweet-level classifier.
 # Each tweet gets the label of the account (even though most tweets are irrelevant to the classification).
 
-# In[3]:
+# In[4]:
 
 max_features = 20000
 maxtweets = 2000
 maxlen = 50  # cut texts to this number of words (among top max_features most common words)
 
-(X_train, y_train), (X_test, y_test) = load_data(nb_words=max_features, maxlen=maxlen)
+# These come out shuffled
+(x_pos, y_pos), (x_neg, y_neg) = load_data(nb_words=max_features, maxlen=maxlen)
+
+# length of the test partition
+pos_len = int(len(y_pos)/10.0)
+neg_len = int(len(y_neg)/10.0)
+
+# This convoluted way of making partitions assures equal pos and neg labels per partition
+pos_test_ids = list(range(pos_len))
+neg_test_ids = list(range(neg_len))
+
+pos_train_ids = list(range(pos_len, len(y_pos)))
+neg_train_ids = list(range(neg_len, len(y_neg)))
+
+X_train = np.append(x_pos[pos_train_ids], x_neg[neg_train_ids])
+y_train = np.append(y_pos[pos_train_ids], y_neg[neg_train_ids])
+
+X_test = np.append(x_pos[pos_test_ids], x_neg[neg_test_ids])
+y_test = np.append(y_pos[pos_test_ids], y_neg[neg_test_ids])
+
+X_train, y_train = shuffle_in_unison(X_train, y_train)
+X_test, y_test = shuffle_in_unison(X_test, y_test)
+
 print(len(X_train), 'train sequences')
 print(len(X_test), 'test sequences')
 
 
-# In[4]:
+# In[5]:
 
 X_train = pad3d(X_train, maxtweets=maxtweets, maxlen=maxlen)
 X_test = pad3d(X_test, maxtweets=maxtweets, maxlen=maxlen)
@@ -286,7 +379,7 @@ print('X_train shape:', train_shp)
 print('X_test shape:', test_shp)
 
 
-# In[7]:
+# In[6]:
 
 X_train_flat = X_train.reshape(train_shp[0] * train_shp[1], train_shp[2])
 y_train_flat = y_train.repeat(train_shp[1])
@@ -300,20 +393,20 @@ y_test_flat = y_test.repeat(test_shp[1])
 X_test_shuff, y_test_shuff = shuffle_in_unison(X_test_flat, y_test_flat)
 
 
-# In[8]:
+# In[ ]:
 
 # just clearing up space -- from now on, we use the flattened representations
 del X_train
 del X_test
 
 
-# In[9]:
+# In[7]:
 
 emb_dim = 200
 embeddings = load_embeddings(nb_words=max_features, emb_dim=emb_dim)
 
 
-# In[10]:
+# In[8]:
 
 nb_filter = 64 # how many convolutional filters
 filter_length = 5 # how many tokens a convolution covers
@@ -322,7 +415,7 @@ nb_epoch = 1 # how many training epochs
 batch_size = 256 # how many tweets to train at a time
 
 
-# In[13]:
+# In[9]:
 
 print('Build first model (tweet-level)...')
 model1 = Sequential()
@@ -349,7 +442,7 @@ model1.compile(loss='binary_crossentropy',
                metrics=['accuracy'])
 
 
-# In[14]:
+# In[10]:
 
 model1.summary()
 
@@ -361,34 +454,34 @@ model1.fit(X_train_shuff, y_train_shuff, batch_size=batch_size, nb_epoch=nb_epoc
            validation_data=(X_test_shuff, y_test_shuff))
 
 
-# In[16]:
+# In[ ]:
 
-#xmodel1.save('tweet_classifier.h5')
-
-
-# In[17]:
-
-#model1 = load_model('tweet_classifier.h5')
+model1.save('tweet_classifier.h5')
 
 
-# In[18]:
+# In[ ]:
+
+model1 = load_model('tweet_classifier.h5')
+
+
+# In[ ]:
 
 score, acc = model1.evaluate(X_test_flat, y_test_flat, batch_size=batch_size)
 
 
-# In[19]:
+# In[ ]:
 
 print('Test score:', score)
 print('Test accuracy:', acc)
 
 
-# In[20]:
+# In[ ]:
 
 pred = model1.predict(X_test_flat)
 pred = pred.reshape((test_shp[0], test_shp[1]))
 
 
-# In[23]:
+# In[ ]:
 
 # account classification with each tweet's classification getting an equal vote
 predmn = np.mean(pred, axis=1)
@@ -400,7 +493,7 @@ predwm = np.average(pred, axis=1, weights=wts)
 predwm = (predwm >= 0.5).astype(int)
 
 
-# In[24]:
+# In[ ]:
 
 y = y_test.flatten()
 
@@ -415,7 +508,7 @@ bootstrap(y, predwm)
 # 
 # Having trained a tweet-level classifier with `model1`, we now create an identical (trained) net except that we cut off final, classifying layer. This allows us to pass forward a 128-length vector for each tweet. The tweets will then be grouped by account (there being a fixed number of tweets per account). The resulting 2-D structure can be passed to a GRU or LSTM for classification.
 
-# In[25]:
+# In[ ]:
 
 intermediate = Sequential()
 intermediate.add(Embedding(max_features + 3, 
@@ -443,12 +536,12 @@ intermediate.compile(loss='binary_crossentropy',
                      metrics=['accuracy'])
 
 
-# In[26]:
+# In[ ]:
 
 intermediate.summary()
 
 
-# In[28]:
+# In[ ]:
 
 X_test_mid = K.eval(intermediate(K.variable(X_test_flat)))
 X_test_mid = X_test_mid.reshape((test_shp[0], test_shp[1], 128))
@@ -459,7 +552,7 @@ X_test_mid = np.fliplr(X_test_mid)
 # 
 # This is the second part of the net, which takes in the 2D account representation and returns an account classification.
 
-# In[29]:
+# In[ ]:
 
 batch_size = 32
 
@@ -477,12 +570,12 @@ model2.compile(loss='binary_crossentropy',
               metrics=['accuracy'])
 
 
-# In[30]:
+# In[ ]:
 
 model2.summary()
 
 
-# In[31]:
+# In[ ]:
 
 chunk = 256
 X_train_mid = np.zeros((train_shp[0], train_shp[1], 128))
@@ -495,7 +588,7 @@ for i in range(0, train_shp[0], chunk):
     X_train_mid[i:(i + last_idx)] = X_train_chunk
 
 
-# In[32]:
+# In[ ]:
 
 model2.fit(X_train_mid,  
            y_train, 
@@ -504,7 +597,7 @@ model2.fit(X_train_mid,
            validation_data=(X_test_mid, y_test))
 
 
-# In[33]:
+# In[ ]:
 
 score, acc = model2.evaluate(X_test_mid, y_test,
                             batch_size=batch_size)
@@ -516,7 +609,7 @@ print('Test accuracy:', acc)
 # 
 # It is possible in some instances to get more or better information from multiple convolution sizes.
 
-# In[37]:
+# In[ ]:
 
 from keras.models import Model
 from keras.layers import Input, MaxPooling1D, Flatten, merge
@@ -566,7 +659,7 @@ towers.compile(loss='binary_crossentropy',
                metrics=['accuracy'])
 
 
-# In[35]:
+# In[ ]:
 
 towers.summary()
 
@@ -584,7 +677,7 @@ score, acc = towers.evaluate(X_test_shuff, y_test_shuff,
                             batch_size=batch_size)
 
 
-# In[40]:
+# In[ ]:
 
 print('Test score:', score)
 print('Test accuracy:', acc)
