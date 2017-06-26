11300dcdd757000bd43ad83d75f757ee02ea61e0
EgoLaparra <egoitz.laparra@ehu.eus>
2017-06-25 18:27:35 -0700

    Prediction with batches

--- owTextClassifier3D_domadapt.py
+++ owTextClassifier3D_domadapt.py
@@ -6,7 +6,7 @@
 # NN models to classify Twitter account users as Overweight or Not Overweight.
 # 
 #
-CUDA_MODE = True
+CUDA_MODE = False
 
 import gzip
 import numpy as np
@@ -84,8 +84,8 @@ def push_indices(x, start, index_from):
     else:
         return x
 
-def load_data(path='ow3df.pkl', nb_words=None, skip_top=0,
-#def load_data(path='data_toy/ow3df.pkl', nb_words=None, skip_top=0,
+#def load_data(path='ow3df.pkl', nb_words=None, skip_top=0,
+def load_data(path='data_toy/ow3df.pkl', nb_words=None, skip_top=0,
               maxlen=None, seed=113, start=1, oov=2, index_from=3):
     '''
     # Arguments
@@ -175,8 +175,8 @@ def load_data(path='ow3df.pkl', nb_words=None, skip_top=0,
 
 
 def load_embeddings(nb_words=None, emb_dim=200, index_from=3,
-                    vocab='ow3df.dict.pkl', 
-                    #vocab='data_toy/ow3df.dict.pkl', 
+                    #vocab='ow3df.dict.pkl', 
+                    vocab='data_toy/ow3df.dict.pkl', 
                     w2v='food_vectors_clean.txt'):
 
     f = open(vocab, 'rb')
@@ -580,27 +580,34 @@ class Pre(nn.Module):
         return out
 
         
-def predict(net, x, f, intermediate=False):
-    fb = torch.LongTensor(torch.np.where(f[:,0]==0)[0])
-    if CUDA_MODE:
-        fb = fb.cuda()
-    xf = x[fb]
-    xf = torch.transpose(xf, 0, 1)
-    mb = torch.LongTensor(torch.np.where(f[:,0]==1)[0])
-    if CUDA_MODE:
-        mb = mb.cuda()
-    xm = x[mb]
-    xm = torch.transpose(xm, 0, 1)
-    f_pred = net(xf, domain=0)
-    m_pred = net(xm, domain=1)
-    fb = fb + 5
-    b = torch.cat((fb, mb))
-    if CUDA_MODE:
-        b = torch.LongTensor(torch.np.argsort(b.cpu().numpy())).cuda()
-    else:
-        b = torch.LongTensor(torch.np.argsort(b.numpy()))    
-    pred = torch.cat((f_pred, m_pred))
-    pred = pred[b]
+def predict(net, x, f, batch_size, intermediate=False):
+    pred = torch.LongTensor()
+    batches = math.ceil(x.size()[0] / batch_size)
+    for b in range(batches):
+        bx = x[b*batch_size:b*batch_size+batch_size]
+        fb = torch.LongTensor(torch.np.where(f[:,0]==0)[0])
+        if CUDA_MODE:
+            fb = fb.cuda()
+        bxf = x[fb]
+        bxf = torch.transpose(bxf, 0, 1)
+        mb = torch.LongTensor(torch.np.where(f[:,0]==1)[0])
+        if CUDA_MODE:
+            mb = mb.cuda()
+        bxm = bx[mb]
+        bxm = torch.transpose(bxm, 0, 1)
+        f_pred = net(bxf, domain=0)
+        m_pred = net(bxm, domain=1)
+        fb = fb + 5
+        cb = torch.cat((fb, mb))
+        if CUDA_MODE:
+            cb = torch.LongTensor(torch.np.argsort(cb.cpu().numpy())).cuda()
+        else:
+            cb = torch.LongTensor(torch.np.argsort(cb.numpy()))    
+        b_pred = torch.cat((f_pred, m_pred))
+        b_pred = b_pred[cb]
+        sys.stdout.write('\r[Predict batch: %3d/%3d]' % (b + 1, batches))
+        sys.stdout.flush()
+        pred = torch.cat(pred, b_pred)
     return pred
     
 def train(net, x, y, f, nepochs, batch_size):
@@ -671,8 +678,8 @@ predictions["cnnw"] = list()
 predictions["gruv"] = list()
 predictions["gruw"] = list()
 gold_test = list()
-foldsfile = "folds.csv"
-#foldsfile = "data_toy/folds.csv"
+#foldsfile = "folds.csv"
+foldsfile = "data_toy/folds.csv"
 for iteration in gen_iterations(pos, neg, max_features, maxtweets, maxlen, foldsfile):
     iterid = iteration[0]
     print('')
@@ -729,7 +736,7 @@ for iteration in gen_iterations(pos, neg, max_features, maxtweets, maxlen, folds
         data_x = Variable(torch.from_numpy(X_dev_flat).long())
     data_f = f_dev_flat
     
-    predDev = predict(net, data_x, data_f)
+    predDev = predict(net, data_x, data_f, batch_size)
     if CUDA_MODE:
         predDev = predDev.cpu().data.numpy().reshape((dev_shp[0], dev_shp[1]))
     else:    
@@ -756,7 +763,7 @@ for iteration in gen_iterations(pos, neg, max_features, maxtweets, maxlen, folds
     else:
         data_x = Variable(torch.from_numpy(X_test_flat).long())
     data_f = f_test_flat
-    predTest = predict(net, data_x, data_f)
+    predTest = predict(net, data_x, data_f, batch_size)
     if CUDA_MODE:
         predTest = predTest.cpu().data.numpy().reshape((test_shp[0], test_shp[1]))
     else:
